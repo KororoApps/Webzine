aa070e38261a9946fdaafd76f73af657bdb589fb
KororoApps <elodie.sponton@yahoo.fr>
2024-02-07 19:23:08 +0100

    #20. Ajout de la fonctionnalité d'affichage des détails des titres. Dans le TitreController, utilisation de la bibliothèque Bogus pour générer des données factices de titres. La vue Details affiche les détails tels que le libellé, la durée, la date de sortie, etc. Le modèle TitreModel définit la liste des titres. L'Entité Titre est créée.

---
+++ Webzine.WebApplication/Areas/Titres/ViewModels/TitreModel.cs
@@ -0,0 +1,15 @@
+﻿using Webzine.Entity;
+
+namespace Webzine.WebApplication.Areas.Titres.ViewModels
+{
+    public class TitreModel
+    {
+        
+            /// <summary>
+            /// Définit la liste des titres
+            /// </summary>
+            public IEnumerable <Titre> Titres { get; set; } = new List<Titre>();
+        }
+    }
+
+
