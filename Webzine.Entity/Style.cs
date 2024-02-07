fc48d87a302786f69ba2b14a06d9bff97d6f360e
KororoApps <elodie.sponton@yahoo.fr>
2024-02-07 20:50:47 +0100

    #22. Ajout de la fonctionnalité d'affichage des libellés des styles de musique. Dans le StyleController, utilisation de la bibliothèque Bogus pour générer des libellés factices de style. La vue Details affiche le style de la musique. Le modèle StyleModel définit la liste des styles de musique. L'Entité Style est créée avec le libellé en champ obligatoire.

---
+++ Webzine.Entity/Style.cs
@@ -0,0 +1,15 @@
+﻿namespace Webzine.Entity
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Style
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Libelle { get; set; }
+    }
+}
---
+++ Webzine.WebApplication/Areas/Styles/Controllers/StyleController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bogus;
+
+
+
+namespace Webzine.WebApplication.Areas.Styles.Controllers
+{
+
+    using Webzine.Entity;
+    using Webzine.WebApplication.Areas.Styles.ViewModels;
+
+    [Area("Styles")]
+        public class StyleController : Controller
+        {
+            public IActionResult Details()
+            {
+                /// <summary>
+                /// Configuration du générateur de fausses données pour la classe Style
+                /// <summary>
+                var styleFaker = new Faker<Style>()
+                    .RuleFor(a => a.Libelle, f => f.Random.Word())
+                    ;
+                /// <summary>
+                /// Génération de 25 fausses instances de la classe Style
+                /// <summary>
+                var styles = styleFaker.Generate(25);
+                /// <summary>
+                /// Création du modèle de vue contenant la liste de styles
+                /// <summary>
+                var styleModel = new StyleModel
+                {
+                    Styles = styles
+                };
+                /// <summary>
+                /// Retour de la vue avec le modèle de vue contenant les styles générés
+                /// <summary>
+                return this.View(styleModel);
+            }
+        }
+    }
+
