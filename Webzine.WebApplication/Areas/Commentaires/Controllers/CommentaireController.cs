f921fa84c136ea81220608cff2eccd5b6eaaa9b5
KororoApps <elodie.sponton@yahoo.fr>
2024-02-08 23:33:00 +0100

    #49. Page de la chronique d'un titre en consultation. Affichage de l'artiste et du titre.

--- Webzine.WebApplication/Areas/Commentaires/Controllers/CommentaireController.cs
+++ Webzine.WebApplication/Areas/Commentaires/Controllers/CommentaireController.cs
@@ -12,28 +12,27 @@ namespace Webzine.WebApplication.Areas.Commentaires.Controllers
         public IActionResult Liste()
         {
             /// <summary>
-            /// Configuration du générateur de fausses données pour la classe Commentaire
+            /// Configuration du générateur de fausses données pour la classe Commentaire.
             /// <summary>
             var commentaireFaker = new Faker<Commentaire>()
                 .RuleFor(p => p.IdCommentaire, f => f.IndexFaker)
                 .RuleFor(p => p.Auteur, f => f.Internet.UserName())
                 .RuleFor(p => p.Contenu, f => f.Lorem.Paragraph())
                 .RuleFor(p => p.DateCreation, f => f.Date.Recent())
-                //.RuleFor(p => p.Titre, f => f.Lorem.Sentence());
             ;
             /// <summary>
-            /// Génération de 5 fausses instances de la classe Commentaire
+            /// Génération de 5 fausses instances de la classe Commentaire.
             /// <summary>
             var commentaires = commentaireFaker.Generate(5);
             /// <summary>
-            /// Création du modèle de vue contenant la liste de commentaires
+            /// Création du modèle de vue contenant la liste de commentaires.
             /// <summary>
             var commentaireModel = new CommentaireModel
             {
                 Commentaires = commentaires
             };
             /// <summary>
-            /// Retour de la vue avec le modèle de vue contenant les commentaires générés
+            /// Retour de la vue avec le modèle de vue contenant les commentaires générés.
             /// <summary>
             return this.View(commentaireModel);
         }
