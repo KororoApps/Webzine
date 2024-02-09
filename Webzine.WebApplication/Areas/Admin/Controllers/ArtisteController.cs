8e7bf46a99623a52e479fa906a5955802604b7d8
KororoApps <elodie.sponton@yahoo.fr>
2024-02-09 14:41:55 +0100

    #257. Supprimer un artiste. Création de la page d'administration de suppression d'un artiste. Intégration du lien pour retourner à l'administration des artistes. Dans la liste des artistes, le lien vers la suppression d'un artiste est effectif.

--- Webzine.WebApplication/Areas/Admin/Controllers/ArtisteController.cs
+++ Webzine.WebApplication/Areas/Admin/Controllers/ArtisteController.cs
@@ -27,7 +27,7 @@ namespace Webzine.WebApplication.Areas.Admin.Controllers
             var artistes = fakerArtiste.Generate(150);
 
             /// <summary>
-            /// Création du modèle de vue contenant la liste de Titres.
+            /// Création du modèle de vue contenant la liste de Artiste.
             /// <summary>
             var artisteModel = new GroupeArtisteModel
             {
@@ -40,6 +40,32 @@ namespace Webzine.WebApplication.Areas.Admin.Controllers
             return this.View(artisteModel);
         }
 
+        public IActionResult Delete()
+        {
+            /// <summary>
+            /// Configuration du générateur de fausses données pour la classe Artiste.
+            /// <summary>
+            var fakerArtiste = new Faker<Artiste>()
+                .RuleFor(a => a.Nom, f => f.Name.FullName());
+
+            /// <summary>
+            /// Génération de 1 fausse instance de la classe Artiste.
+            /// <summary>//
+            var artiste = fakerArtiste.Generate();
+
+            /// <summary>
+            /// Création du modèle de vue contenant la liste de Artiste.
+            /// <summary>
+            var artisteModel = new ArtisteModel
+            {
+                Artiste = artiste
+            };
+
+            /// <summary>
+            /// Retour de la vue avec le modèle de vue contenant les titres générés.
+            /// <summary>
+            return this.View(artisteModel);
+        }
 
     }
 }
