c42e6ab115509284e7e5878082749f29c12fbaef
KororoApps <elodie.sponton@yahoo.fr>
2024-02-09 15:06:57 +0100

    #258. Modifier un artiste. Génération d'un formulaire pour modifier un artiste dans la page administration. Intégration du lien "Retour à l'administration des artistes". Dans la page Liste des Artiste, lien vers la page Modification d'un artiste en cliquant sur le bouton l'icône crayon.

--- Webzine.WebApplication/Areas/Admin/Controllers/ArtisteController.cs
+++ Webzine.WebApplication/Areas/Admin/Controllers/ArtisteController.cs
@@ -77,5 +77,33 @@ namespace Webzine.WebApplication.Areas.Admin.Controllers
             return this.View();
         }
 
+        public IActionResult Update()
+        {
+            /// <summary>
+            /// Configuration du générateur de fausses données pour la classe Artiste.
+            /// <summary>
+            var fakerArtiste = new Faker<Artiste>()
+                .RuleFor(a => a.Nom, f => f.Name.FullName())
+                .RuleFor(a => a.Biographie, (f, u) => f.Lorem.Paragraph());
+
+            /// <summary>
+            /// Génération de 1 fausse instance de la classe Artiste.
+            /// <summary>//
+            var artiste = fakerArtiste.Generate();
+
+            /// <summary>
+            /// Création du modèle de vue contenant la liste de Artiste.
+            /// <summary>
+            var artisteModel = new ArtisteModel
+            {
+                Artiste = artiste
+            };
+
+            /// <summary>
+            /// Retour de la vue avec le modèle de vue contenant les titres générés.
+            /// <summary>
+            return this.View(artisteModel);
+        }
+
     }
 }
