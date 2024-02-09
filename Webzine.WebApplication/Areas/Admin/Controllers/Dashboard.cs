3880e8c7be9ce77c02215cea8809930982b5945c
LucasFernandez <lucas.fernandez14102002@gmail.com>
2024-02-09 17:16:40 +0100

    #117 #118 #119 #120 #121 #122 #123 #124 #125 #126

---
+++ Webzine.WebApplication/Areas/Admin/Controllers/Dashboard.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bogus;
+
+
+namespace Webzine.WebApplication.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Webzine.Entity;
+    using Webzine.WebApplication.Areas.Admin.ViewModels;
+    using Webzine.WebApplication.Areas.Titres.ViewModels;
+
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
