53aed3edb1abf9700c5f9b7371080c269d26ad29
KororoApps <elodie.sponton@yahoo.fr>
2024-02-10 22:44:35 +0100

    StyleCop. Correction apportée sur le .csproj suite à la mise en placec du stylecop qui provoquait une erreur de compilation.

--- Webzine.Entity/Artiste.cs
+++ Webzine.Entity/Artiste.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Webzine.Entity
+﻿namespace Webzine.Entity
 {
+
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
     public class Artiste
     {
         public int IdArtiste { get; set; }
