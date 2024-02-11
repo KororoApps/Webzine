8546812112bae475ecd1a7d25cbe48a807858be0
KororoApps <elodie.sponton@yahoo.fr>
2024-02-11 13:21:02 +0100

    #252. Modifier un style. Ajout de la page de modification d'un style de musique. #251, #253, #254, Correction des pages d'ajout, modification et suppression d'un style de musique afin d'intégrer des fausses données lorsque c'est nécessaire et d'intégrer les liens vers les autres pages. Ajout de commentaires pour l'entièreté du CRUD Style : controller, view et viewModels.

--- Webzine.WebApplication/Shared/ViewModels/StyleModel.cs
+++ Webzine.WebApplication/Shared/ViewModels/StyleModel.cs
@@ -1,12 +1,19 @@
-﻿using Webzine.Entity;
+﻿// <copyright file="GroupeStyleModel.cs" company="Equipe 4 - Andgel Sassignol, Romain Vidotto, Jean-Emilien Viard, Lucas Fernandez, Dylann-Nick Etou Mbon, Antoine Couvert, Elodie Sponton">
+// Copyright (c) Equipe 4 - Andgel Sassignol, Romain Vidotto, Jean-Emilien Viard, Lucas Fernandez, Dylann-Nick Etou Mbon, Antoine Couvert, Elodie Sponton. All rights reserved.
+// </copyright>
+
 namespace Webzine.WebApplication.Shared.ViewModels
 {
+    using Webzine.Entity;
+
+    /// <summary>
+    /// Représente le modèle de vue pour un style.
+    /// </summary>
     public class StyleModel
     {
-
         /// <summary>
-        /// Définit la liste des styles
+        /// Obtient ou définit le style.
         /// </summary>
-        public IEnumerable<Style> Styles { get; set; } = new List<Style>();
+        public Style Style { get; set; } = null!;
     }
 }
