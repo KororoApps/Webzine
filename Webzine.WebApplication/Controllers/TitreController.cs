e7b4163b8620803f5283a3062e146eb763fb261f
KororoApps <elodie.sponton@yahoo.fr>
2024-02-11 19:59:52 +0100

    #10. Page d'un titre. Mise en forme du front de la page d'accueil et mise en forme conventionnelle du code. Encore à faire : Activer le lien vers les styles, Mettre correctement en forme la Chronique/Jaquette.

--- Webzine.WebApplication/Controllers/TitreController.cs
+++ Webzine.WebApplication/Controllers/TitreController.cs
@@ -9,11 +9,21 @@ namespace Webzine.WebApplication.Controllers
     using Webzine.Entity;
     using Webzine.WebApplication.Shared.ViewModels;
 
+    /// <summary>
+    /// Contrôleur responsable de la gestion des titres.
+    /// </summary>
+    /// <remarks>
+    /// Ce contrôleur gère l'affichage de la chronique d'un titre.
+    /// Il utilise le générateur de fausses données Bogus pour simuler des données.
+    /// </remarks>
     public class TitreController : Controller
     {
+        /// <summary>
+        /// Action qui affiche la liste des titres.
+        /// </summary>
+        /// <returns>Vue avec la liste des titres générés.</returns>
         public IActionResult Index()
         {
-
             /// <summary>
             /// Configuration du générateur de fausses données pour la classe Artiste.
             /// <summary>
@@ -42,7 +52,7 @@ namespace Webzine.WebApplication.Controllers
             /// Configuration du générateur de fausses données pour la classe Style.
             /// <summary>
             var fakerStyle = new Faker<Style>()
-                .RuleFor(a => a.Libelle, f => f.Random.Word());
+                .RuleFor(a => a.Libelle, f => f.Random.Words(1));
 
             /// <summary>
             /// Génération de 3 fausses instances de la classe Style.
@@ -80,13 +90,91 @@ namespace Webzine.WebApplication.Controllers
             /// <summary>
             var titreModel = new TitreModel
             {
-                Titre = titre
+                Titre = titre,
             };
 
             /// <summary>
             /// Retour de la vue avec le modèle de vue contenant les titres générés.
             /// <summary>
-            return View(titreModel);
+            return this.View(titreModel);
+        }
+
+
+        public IActionResult Style()
+        {
+            /// <summary>
+            /// Configuration du générateur de fausses données pour la classe Artiste.
+            /// <summary>
+            var fakerArtiste = new Faker<Artiste>()
+                .RuleFor(a => a.Nom, f => f.Name.FullName());
+
+            /// <summary>
+            /// Génération de 1 fausse instance de la classe Artiste.
+            /// <summary>//
+            var artiste = fakerArtiste.Generate();
+
+            /// <summary>
+            /// Configuration du générateur de fausses données pour la classe Commentaire.
+            /// <summary>
+            var fakerCommentaire = new Faker<Commentaire>()
+                .RuleFor(c => c.Auteur, f => f.Name.FullName())
+                .RuleFor(c => c.Contenu, f => f.Lorem.Paragraph())
+                .RuleFor(c => c.DateCreation, f => f.Date.Recent());
+
+            /// <summary>
+            /// Génération de 3 fausses instances de la classe Commentaire.
+            /// <summary>//
+            var commentaires = fakerCommentaire.Generate(3);
+
+            /// <summary>
+            /// Configuration du générateur de fausses données pour la classe Style.
+            /// <summary>
+            var fakerStyle = new Faker<Style>()
+                .RuleFor(a => a.Libelle, f => f.Random.Words(1));
+
+            /// <summary>
+            /// Génération de 3 fausses instances de la classe Style.
+            /// <summary>//
+            var styles = fakerStyle.Generate(2);
+
+            /// <summary>
+            /// Configuration du générateur de fausses données pour la classe Titre.
+            /// <summary>
+            var titreFaker = new Faker<Titre>()
+                .RuleFor(t => t.IdTitre, f => f.IndexFaker)
+                .RuleFor(t => t.Libelle, f => f.Name.FullName())
+                .RuleFor(t => t.Duree, f => f.Date.Timespan())
+                .RuleFor(t => t.DateSortie, f => f.Date.Past())
+                .RuleFor(t => t.DateCreation, f => f.Date.Recent())
+                .RuleFor(t => t.NbLectures, f => f.Random.Number(1, 10000))
+                .RuleFor(t => t.NbLikes, f => f.Random.Number(1, 1000))
+                .RuleFor(t => t.Album, f => f.Commerce.ProductName())
+                .RuleFor(t => t.Chronique, f => f.Lorem.Paragraphs(10))
+                .RuleFor(t => t.UrlJaquette, f => f.Image.PicsumUrl().ToString())
+                .RuleFor(t => t.UrlEcoute, f => f.Internet.Url())
+                .RuleFor(t => t.Album, f => f.Commerce.ProductName())
+                ;
+
+            /// <summary>
+            /// Génération de 1 fausse instance de la classe Titre.
+            /// <summary>
+            var titre = titreFaker.Generate();
+            titre.Artiste = artiste;
+            titre.Commentaires = commentaires;
+            titre.Styles = styles;
+
+            /// <summary>
+            /// Création du modèle de vue contenant la liste de Titres.
+            /// <summary>
+            var titreModel = new TitreModel
+            {
+                Titre = titre,
+            };
+
+            /// <summary>
+            /// Retour de la vue avec le modèle de vue contenant les titres générés.
+            /// <summary>
+            return this.View(titreModel);
         }
     }
 }
