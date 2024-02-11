65fec9d404487cf93151520fcdc4431af8fb9d85
KororoApps <elodie.sponton@yahoo.fr>
2024-02-11 20:37:18 +0100

    #259. Titres ayant le même style. Création de la page renvoyant aux titres ayant le même style. Intégration des liens vers cette page dans la page d'accueil et dans la page chronique.

--- Webzine.WebApplication/Controllers/TitreController.cs
+++ Webzine.WebApplication/Controllers/TitreController.cs
@@ -99,7 +99,10 @@ namespace Webzine.WebApplication.Controllers
             return this.View(titreModel);
         }
 
-
+        /// <summary>
+        /// Action permettant d'afficher les titres liés à un style.
+        /// </summary>
+        /// <returns>Vue contenant la liste des titres liés au style.</returns>
         public IActionResult Style()
         {
             /// <summary>
@@ -111,64 +114,29 @@ namespace Webzine.WebApplication.Controllers
             /// <summary>
             /// Génération de 1 fausse instance de la classe Artiste.
             /// <summary>//
-            var artiste = fakerArtiste.Generate();
-
-            /// <summary>
-            /// Configuration du générateur de fausses données pour la classe Commentaire.
-            /// <summary>
-            var fakerCommentaire = new Faker<Commentaire>()
-                .RuleFor(c => c.Auteur, f => f.Name.FullName())
-                .RuleFor(c => c.Contenu, f => f.Lorem.Paragraph())
-                .RuleFor(c => c.DateCreation, f => f.Date.Recent());
-
-            /// <summary>
-            /// Génération de 3 fausses instances de la classe Commentaire.
-            /// <summary>//
-            var commentaires = fakerCommentaire.Generate(3);
-
-            /// <summary>
-            /// Configuration du générateur de fausses données pour la classe Style.
-            /// <summary>
-            var fakerStyle = new Faker<Style>()
-                .RuleFor(a => a.Libelle, f => f.Random.Words(1));
-
-            /// <summary>
-            /// Génération de 3 fausses instances de la classe Style.
-            /// <summary>//
-            var styles = fakerStyle.Generate(2);
+            var artistes = fakerArtiste.Generate(150);
 
             /// <summary>
             /// Configuration du générateur de fausses données pour la classe Titre.
             /// <summary>
             var titreFaker = new Faker<Titre>()
-                .RuleFor(t => t.IdTitre, f => f.IndexFaker)
                 .RuleFor(t => t.Libelle, f => f.Name.FullName())
-                .RuleFor(t => t.Duree, f => f.Date.Timespan())
-                .RuleFor(t => t.DateSortie, f => f.Date.Past())
-                .RuleFor(t => t.DateCreation, f => f.Date.Recent())
-                .RuleFor(t => t.NbLectures, f => f.Random.Number(1, 10000))
-                .RuleFor(t => t.NbLikes, f => f.Random.Number(1, 1000))
-                .RuleFor(t => t.Album, f => f.Commerce.ProductName())
-                .RuleFor(t => t.Chronique, f => f.Lorem.Paragraphs(10))
+                .RuleFor(t => t.Duree, f => TimeSpan.FromSeconds(f.Random.Number(60, 600)))
+                .RuleFor(t => t.Artiste, f => f.PickRandom(artistes))
                 .RuleFor(t => t.UrlJaquette, f => f.Image.PicsumUrl().ToString())
-                .RuleFor(t => t.UrlEcoute, f => f.Internet.Url())
-                .RuleFor(t => t.Album, f => f.Commerce.ProductName())
                 ;
 
             /// <summary>
-            /// Génération de 1 fausse instance de la classe Titre.
+            /// Génération de 500 fausse instance de la classe Titre.
             /// <summary>
-            var titre = titreFaker.Generate();
-            titre.Artiste = artiste;
-            titre.Commentaires = commentaires;
-            titre.Styles = styles;
+            var titres = titreFaker.Generate(500);
 
             /// <summary>
             /// Création du modèle de vue contenant la liste de Titres.
             /// <summary>
-            var titreModel = new TitreModel
+            var titreModel = new GroupeTitreModel
             {
-                Titre = titre,
+                Titres = titres,
             };
 
             /// <summary>
