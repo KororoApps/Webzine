6bd3f5a29918f62a2a645be4d650d426781f1ea7
Dylann-Nick <147245156+Dylann-Nick@users.noreply.github.com>
2024-02-12 20:58:50 +0100

    Responssive design

--- Webzine.WebApplication/Controllers/ContactController.cs
+++ Webzine.WebApplication/Controllers/ContactController.cs
@@ -15,17 +15,14 @@ namespace Webzine.WebApplication.Controllers
             string CompanyName = "C.U.C.D.B - DIIAGE";
             string Address = "69 Avenue Aristide Briand";
             string VilleDepartement = "21000 Dijon";
-            string Phone = "03 80 73 45 90";
-            string Mail = "secretariat@cucdb.fr";
+           
             ContactModel contacts = new()
             {
                 listString =
                 [
                     CompanyName,
                     Address,
-                    VilleDepartement,
-                    Phone,
-                    Mail,
+                    VilleDepartement
                 ],
             };
             /// <summary>
