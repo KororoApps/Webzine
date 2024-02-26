ccca7d1b176b037041c160360c068c38ee1db006
Antoine_Couvert <antoine.couvert@diiage.org>
2024-02-26 20:13:54 +0100

    Interfaces Faites

---
+++ Webzine.Repository.Contracts/IArtisteRepository.cs
@@ -0,0 +1,13 @@
+﻿using Webzine.Entity;
+
+namespace Webzine.Repository.Contracts
+{
+    public interface IArtisteRepository
+    {
+        void Add(Artiste artiste);
+        void Delete(Artiste artiste);
+        Artiste Find(int id);
+        IEnumerable<Artiste> FindAll();
+        void Update(Artiste artiste);
+    }
+}
---
+++ Webzine.Repository.Contracts/ICommentaireRepository.cs
@@ -0,0 +1,12 @@
+﻿using Webzine.Entity;
+
+namespace Webzine.Repository.Contracts
+{
+    public interface ICommentaireRepository
+    {
+        void Add(Commentaire commentaire);
+        void Delete(Commentaire commentaire);
+        Commentaire Find(int id);
+        IEnumerable<Commentaire> FindAll();
+    }
+}
