3ef13b92d0ac22f4a6ff577bf3c93e92ed6805c1
Antoine_Couvert <antoine.couvert@diiage.org>
2024-02-29 14:00:46 +0100

    Code commenté

--- Webzine.Repository/CommentaireRepository.cs
+++ Webzine.Repository/CommentaireRepository.cs
@@ -17,17 +17,30 @@ namespace Webzine.Repository
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Supprimme un commentaire de la base de donnée
+        /// </summary>
+        /// <param name="commentaire"></param>
         public void Delete(Commentaire commentaire)
         {
             _context.Remove(commentaire);
         }
 
+        /// <summary>
+        ///Renvoie le premier commentaire ayant l'id mise en paramètre
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Commentaire Find(int id)
         {
             var Commentaire = _context.Commentaires.Include(c => c.Titre).Where(c => c.IdCommentaire == id).First();
             return Commentaire;
         }
 
+        /// <summary>
+        /// Renvoie tout les commentaires
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<Commentaire> FindAll()
         {
             var allCommentaires = _context.Commentaires.Include(c => c.Titre).OrderBy(c => c.IdTitre).ThenBy(c => c.DateCreation).ToList();
