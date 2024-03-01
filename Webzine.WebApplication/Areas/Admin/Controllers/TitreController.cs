fdef2967cbed2210850c23b7a2abf9a40a7b52a0
Antoine_Couvert <antoine.couvert@diiage.org>
2024-03-01 11:19:23 +0100

    TitreController n'implémentant pas le repository corrigé

--- Webzine.WebApplication/Areas/Admin/Controllers/TitreController.cs
+++ Webzine.WebApplication/Areas/Admin/Controllers/TitreController.cs
@@ -7,6 +7,7 @@ namespace Webzine.WebApplication.Areas.Admin.Controllers
     using Microsoft.AspNetCore.Mvc;
     using Webzine.Entity;
     using Webzine.Entity.Fixtures;
+    using Webzine.Repository.Contracts;
     using Webzine.WebApplication.Shared.ViewModels;
 
     /// <summary>
@@ -19,24 +20,22 @@ namespace Webzine.WebApplication.Areas.Admin.Controllers
     [Area("Admin")]
     public class TitreController : Controller
     {
+        ITitreRepository _titreRepository;
 
+        public TitreController(ITitreRepository titreRepository)
+        {
+            this._titreRepository = titreRepository;
+        }
         /// <summary>
         /// Affiche la liste des titres.
         /// </summary>
         /// <returns>Vue avec la liste des titres générés.</returns>
         public IActionResult Index()
         {
-            // Génération d'une liste de titres.
-            List<Titre> titres = DataFactory.Titres;
-
-
-            // Tri de la liste des titres par date de création.
-            var titresTries = titres.OrderByDescending(t => t.DateSortie).ToList();
-
             // Création du modèle de vue contenant la liste de Titres.
             var titreModel = new GroupeTitreModel
             {
-                Titres = titresTries,
+                Titres = this._titreRepository.FindAll(),
             };
 
             // Retour de la vue avec le modèle de vue contenant les titres générés.
@@ -47,17 +46,12 @@ namespace Webzine.WebApplication.Areas.Admin.Controllers
         /// Affiche la vue de suppression d'un titre.
         /// </summary>
         /// <returns>Vue de suppression d'un titre.</returns>
-        public IActionResult Delete()
+        public IActionResult Delete(int id)
         {
-
-            // Génération d'un titre.
-            List<Titre> titres = DataFactory.Titres;
-            Titre titre = titres.OrderBy(t => Guid.NewGuid()).FirstOrDefault();
-
             // Création du modèle de vue contenant un Titre.
             var titreModel = new TitreModel
             {
-                Titre = titre,
+                Titre = this._titreRepository.Find(id),
             };
 
             // Retour de la vue avec le modèle de vue contenant le titre généré.
@@ -73,6 +67,7 @@ namespace Webzine.WebApplication.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult DeleteConfirmed(int id)
         {
+            this._titreRepository.Delete(this._titreRepository.Find(id));
             return this.RedirectToAction(nameof(this.Index));
         }
 
