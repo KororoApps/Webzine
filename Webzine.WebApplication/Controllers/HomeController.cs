5e1d94826431c6314b202517a245643b6b1399b8
KororoApps <elodie.sponton@yahoo.fr>
2024-03-01 19:49:48 +0100

    HomeController. Correction du fichier.

--- Webzine.WebApplication/Controllers/HomeController.cs
+++ Webzine.WebApplication/Controllers/HomeController.cs
@@ -5,22 +5,20 @@
 namespace Webzine.WebApplication.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
-    using Webzine.Entity;
-    using Webzine.Entity.Fixtures;
     using Webzine.Repository.Contracts;
     using Webzine.WebApplication.Shared.ViewModels;
 
     /// <summary>
     /// Contrôleur principal gérant les actions liées à la page d'accueil.
     /// </summary>
-    public class HomeController : Controller
+    /// <remarks>
+    /// Initialise une nouvelle instance de la classe <see cref="HomeController"/>.
+    /// </remarks>
+    /// <param name="titreRepository">Le repository des titres.</param>
+    public class HomeController(ITitreRepository titreRepository) : Controller
     {
-        private readonly ITitreRepository _titreRepository;
+        private readonly ITitreRepository titreRepository = titreRepository;
 
-        public HomeController(ITitreRepository titreRepository)
-        {
-            _titreRepository = titreRepository;
-        }
         /// <summary>
         /// Affiche la page d'accueil.
         /// </summary>
@@ -30,7 +28,7 @@ namespace Webzine.WebApplication.Controllers
             // Création du modèle de vue contenant la liste des titres.
             GroupeTitreModel groupeTitreModel = new()
             {
-                Titres = this._titreRepository.FindAll(),
+                Titres = this.titreRepository.FindAll(),
             };
 
             // Retour de la vue avec le modèle de vue contenant les détails des titres.
