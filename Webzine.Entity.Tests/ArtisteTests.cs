10e5f3f84a6a238827281096edf611a58fc2c639
KororoApps <elodie.sponton@yahoo.fr>
2024-03-02 03:36:48 +0100

    Ajout de commentaires pour les fichiers Tests.

--- Webzine.Entity.Tests/ArtisteTests.cs
+++ Webzine.Entity.Tests/ArtisteTests.cs
@@ -10,42 +10,63 @@ namespace Webzine.Entity.Tests
     [TestClass]
     public class ArtisteTests
     {
+        /// <summary>
+        /// Vérifie que la propriété IdArtiste existe.
+        /// </summary>
         [TestMethod]
         public void ArtisteHasIdArtiste()
         {
             Common.HasProperty(typeof(Artiste), nameof(Artiste.IdArtiste));
         }
 
+        /// <summary>
+        /// Vérifie que la propriété Nom existe.
+        /// </summary>
         [TestMethod]
         public void ArtisteHasNom()
         {
             Common.HasProperty(typeof(Artiste), nameof(Artiste.Nom));
         }
 
+        /// <summary>
+        /// Vérifie que l'attribut LongueurMin pour la propriété Nom est correct.
+        /// </summary>
         [TestMethod]
         public void ArtisteHasNomTailleMin1()
         {
             Common.AttributLongueurMin(typeof(Artiste), nameof(Artiste.Nom), 2);
         }
 
+        /// <summary>
+        /// Vérifie que l'attribut LongueurMax pour la propriété Nom est correct.
+        /// </summary>
         [TestMethod]
         public void ArtisteHasNomTailleMax50()
         {
             Common.AttributLongueurMax(typeof(Artiste), nameof(Artiste.Nom), 50);
         }
 
+        /// <summary>
+        /// Vérifie que l'attribut Display pour la propriété Nom est correct.
+        /// </summary>
         [TestMethod]
         public void ArtisteHasNomDisplayValid()
         {
             Common.AttributDisplay(typeof(Artiste), nameof(Artiste.Nom), "Nom de l'artiste");
         }
 
+        /// <summary>
+        /// Vérifie que la propriété Biographie existe.
+        /// </summary>
         [TestMethod]
         public void ArtisteHasBiographie()
         {
             Common.HasProperty(typeof(Artiste), nameof(Artiste.Biographie));
         }
 
+        /// <summary>
+        /// Vérifie que la propriété Titres existe.
+        /// </summary>
         [TestMethod]
         public void ArtisteHasTitres()
         {
--- Webzine.Entity.Tests/CommentaireTests.cs
+++ Webzine.Entity.Tests/CommentaireTests.cs
@@ -10,96 +10,144 @@
     [TestClass]
     public class CommentaireTests
     {
+        /// <summary>
+        /// Vérifie que la propriété IdCommentaire existe.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasIdCommentaire()
         {
             Common.HasProperty(typeof(Commentaire), nameof(Commentaire.IdCommentaire));
         }
 
+        /// <summary>
+        /// Vérifie que la propriété Contenu existe.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasContenu()
         {
             Common.HasProperty(typeof(Commentaire), nameof(Commentaire.Contenu));
         }
 
+        /// <summary>
+        /// Vérifie que l'attribut Display pour la propriété Contenu est correct.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasContenuDisplayValid()
         {
             Common.AttributDisplay(typeof(Commentaire), nameof(Commentaire.Contenu), "Commentaire");
         }
 
+        /// <summary>
+        /// Vérifie que l'attribut Requis pour la propriété Contenu est correct.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasContenuRequis()
         {
             Common.AttributRequis(typeof(Commentaire), nameof(Commentaire.Contenu));
         }
 
+        /// <summary>
+        /// Vérifie que l'attribut LongueurMin pour la propriété Contenu est correct.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasContenuTailleMin10()
         {
             Common.AttributLongueurMin(typeof(Commentaire), nameof(Commentaire.Contenu), 10);
         }
 
+        /// <summary>
+        /// Vérifie que l'attribut LongueurMax pour la propriété Contenu est correct.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasContenuTailleMax1000()
         {
             Common.AttributLongueurMax(typeof(Commentaire), nameof(Commentaire.Contenu), 1000);
         }
 
+        /// <summary>
+        /// Vérifie que la propriété Auteur existe.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasAuteur()
         {
             Common.HasProperty(typeof(Commentaire), nameof(Commentaire.Auteur));
         }
 
+        /// <summary>
+        /// Vérifie que l'attribut Display pour la propriété Auteur est correct.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasAuteurDisplayValid()
         {
             Common.AttributDisplay(typeof(Commentaire), nameof(Commentaire.Auteur), "Nom");
         }
 
+        /// <summary>
+        /// Vérifie que l'attribut Requis pour la propriété Auteur est correct.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasAuteurRequis()
         {
             Common.AttributRequis(typeof(Commentaire), nameof(Commentaire.Auteur));
         }
 
+        /// <summary>
+        /// Vérifie que l'attribut LongueurMin pour la propriété Auteur est correct.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasAuteurTailleMin2()
         {
             Common.AttributLongueurMin(typeof(Commentaire), nameof(Commentaire.Auteur), 2);
         }
 
+        /// <summary>
+        /// Vérifie que l'attribut LongueurMax pour la propriété Auteur est correct.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasAuteurTailleMax30()
         {
             Common.AttributLongueurMax(typeof(Commentaire), nameof(Commentaire.Auteur), 30);
         }
 
+        /// <summary>
+        /// Vérifie que la propriété DateCreation existe.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasDateCreation()
         {
             Common.HasProperty(typeof(Commentaire), nameof(Commentaire.DateCreation));
         }
 
+        /// <summary>
+        /// Vérifie que l'attribut Requis pour la propriété DateCreation est correct.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasDateCreationRequis()
         {
             Common.AttributRequis(typeof(Commentaire), nameof(Commentaire.DateCreation));
         }
 
+        /// <summary>
+        /// Vérifie que l'attribut Display pour la propriété DateCreation est correct.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasDateCreationDisplayValid()
         {
             Common.AttributDisplay(typeof(Commentaire), nameof(Commentaire.DateCreation), "Date de création");
         }
 
+        /// <summary>
+        /// Vérifie que la propriété IdTitre existe.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasIdTitre()
         {
             Common.HasProperty(typeof(Commentaire), nameof(Commentaire.IdTitre));
         }
 
+        /// <summary>
+        /// Vérifie que la propriété Titre existe.
+        /// </summary>
         [TestMethod]
         public void CommentaireHasTitre()
         {
