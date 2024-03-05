be22f68e0b2eeb05a5429f0bd32300279df40f70
KororoApps <elodie.sponton@yahoo.fr>
2024-03-05 09:14:15 +0100

    SeedData. Renommé en SeedDataLocal et placé dans un dossier Seeders.

---
+++ Webzine.EntitiesContext/Seeders/SeedDataLocal.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Webzine.Entity.Fixtures;
+
+namespace Webzine.EntitiesContext.Seeders
+{
+    public static class SeedDataLocal
+    {
+        public static void Initialize(IServiceProvider serviceProvider, WebzineDbContext context)
+        {
+
+            // Désactivez le suivi des changements 
+            context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+
+            context.Artistes.AddRange(DataFactory.Artistes);
+            context.Commentaires.AddRange(DataFactory.Commentaires);
+            context.Styles.AddRange(DataFactory.Styles);
+            context.Titres.AddRange(DataFactory.Titres);
+
+            // SaveChanges après le seeding
+            context.SaveChanges();
+
+        }
+
+    }
+}
+
+
