c774a0f67ec1760a7fabcb87cc0e7b83be9895e6
KororoApps <elodie.sponton@yahoo.fr>
2024-03-05 22:41:06 +0100

    #435. LocaArtisteRepository. Correction de l'édition d'un artiste.

--- Webzine.Repository/LocalArtisteRepository.cs
+++ Webzine.Repository/LocalArtisteRepository.cs
@@ -80,9 +80,13 @@ namespace Webzine.Repository
         /// <inheritdoc />
         public void Update(Artiste artiste)
         {
-            if (artiste == null)
+            var artisteAEditer = DataFactory.Artistes
+                .FirstOrDefault(s => s.IdArtiste == artiste.IdArtiste);
+
+            if (artisteAEditer != null)
             {
-                throw new ArgumentNullException(nameof(artiste));
+                artisteAEditer.Nom = artiste.Nom;
+                artisteAEditer.Biographie = artiste.Biographie;
             }
         }
 
@@ -110,8 +114,8 @@ namespace Webzine.Repository
             var artiste = DataFactory.Artistes
                 .Where(a => a.Titres != null && a.Titres.Count != 0)
                 .OrderByDescending(a => a.Titres
-                .GroupBy(t => new { t.Album, t.Artiste }) 
-                .Count()) 
+                .GroupBy(t => new { t.Album, t.Artiste })
+                .Count())
                 .FirstOrDefault();
 
             if (artiste != null)
