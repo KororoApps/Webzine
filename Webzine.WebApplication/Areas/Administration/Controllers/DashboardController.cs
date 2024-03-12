d1ef6b20f739878695560d9e4f9b7107459d9bfb
KororoApps <elodie.sponton@yahoo.fr>
2024-03-12 12:47:25 +0100

    #426. DashboardController. Ajout de commentaires?

--- Webzine.WebApplication/Areas/Administration/Controllers/DashboardController.cs
+++ Webzine.WebApplication/Areas/Administration/Controllers/DashboardController.cs
@@ -9,8 +9,13 @@ namespace Webzine.WebApplication.Areas.Administration.Controllers
     using Webzine.WebApplication.Shared.ViewModels;
 
     /// <summary>
-    /// Contrôleur responsable de la gestion du tableau de bord Administrationistratif.
+    /// Contrôleur responsable de la gestion des opérations liées au dashboard dans la zone d'administration.
     /// </summary>
+    /// <remarks>
+    /// Ce contrôleur gère l'affichage du dashboard .
+    /// Il utilise soit le générateur de fausses données Bogus pour simuler des données, soit des données Spotify en fonction du Seeder sélectionné.
+    /// </remarks>
+    /// <param name="dashboardService">Référence à la classe responsable de l'accès aux données du dashboard.</param>
     [Area("Administration")]
     public class DashboardController(IDashboardService dashboardService) : Controller
     {
@@ -22,17 +27,18 @@ namespace Webzine.WebApplication.Areas.Administration.Controllers
         /// <returns>Vue du dashboard.</returns>
         public IActionResult Index()
         {
-            var model = new DashboardModel();
-
-            model.TitrePlusLu = this.dashboardService.FindTitreLePlusLu();
-            model.NbTitres = this.dashboardService.NombreTitres();
-            model.NbStyles = this.dashboardService.NombreStyles();
-            model.NbArtistes = this.dashboardService.NombreArtistes();
-            model.ArtistePlusDeTitres = this.dashboardService.FindArtisteLePlusTitresAlbumDistinct();
-            model.NbBioArtiste = this.dashboardService.NombreBiographiesArtistes();
-            model.NbLikes = this.dashboardService.NombreLikes();
-            model.NbLectures = this.dashboardService.NombreLectures();
-            model.ArtistePlusChronique = this.dashboardService.FindArtisteLePlusChronique();
+            var model = new DashboardModel
+            {
+                TitrePlusLu = this.dashboardService.FindTitreLePlusLu(),
+                NbTitres = this.dashboardService.NombreTitres(),
+                NbStyles = this.dashboardService.NombreStyles(),
+                NbArtistes = this.dashboardService.NombreArtistes(),
+                ArtistePlusDeTitres = this.dashboardService.FindArtisteLePlusTitresAlbumDistinct(),
+                NbBioArtiste = this.dashboardService.NombreBiographiesArtistes(),
+                NbLikes = this.dashboardService.NombreLikes(),
+                NbLectures = this.dashboardService.NombreLectures(),
+                ArtistePlusChronique = this.dashboardService.FindArtisteLePlusChronique(),
+            };
 
             return this.View(model);
         }
