8934ec597139b6ab3602459b90f00749534e19ef
KororoApps <elodie.sponton@yahoo.fr>
2024-03-12 10:47:11 +0100

    #426/ GlobalUsings. Ajout de commentaires.

--- Webzine.Business.Tests/GlobalUsings.cs
+++ Webzine.Business.Tests/GlobalUsings.cs
@@ -1,1 +1,6 @@
+// <copyright file="GlobalUsings.cs" company="Equipe 4 - Andgel Sassignol, Romain Vidotto, Jean-Emilien Viard, Lucas Fernandez, Dylann-Nick Etou Mbon, Antoine Couvert, Elodie Sponton">
+// Copyright (c) Equipe 4 - Andgel Sassignol, Romain Vidotto, Jean-Emilien Viard, Lucas Fernandez, Dylann-Nick Etou Mbon, Antoine Couvert, Elodie Sponton. All rights reserved.
+// </copyright>
+
+// Utilisation globale nécessaire pour les tests unitaires avec MSTest.
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
