a28a183d92d319203a8b0018e88a0c90cbb97618
KororoApps <elodie.sponton@yahoo.fr>
2024-03-12 10:18:51 +0100

    #426. DashboardService. Correction du code.

--- Webzine.Business/DashboardService.cs
+++ Webzine.Business/DashboardService.cs
@@ -1,43 +1,47 @@
-﻿using Webzine.Business.Contracts;
-using Webzine.Entity;
-using Webzine.Repository.Contracts;
-
-namespace Webzine.Business
+﻿namespace Webzine.Business
 {
-    public class DashboardService : IDashboardService
+    using Webzine.Business.Contracts;
+    using Webzine.Entity;
+    using Webzine.Repository.Contracts;
+
+    /// <summary>
+    /// Implémentation de l'interface IDashboardService pour la récupération d'informations liées au tableau de bord.
+    /// </summary>
+    /// <remarks>
+    /// Initialise une nouvelle instance de la classe <see cref="DashboardService"/>.
+    /// Constructeur de DashboardService prenant les dépendances du repository en tant que paramètres.
+    /// </remarks>
+    /// <param name="titreRepository">Le repository pour les opérations liées aux titres.</param>
+    /// <param name="styleRepository">Le repository pour les opérations liées aux styles.</param>
+    /// <param name="artisteRepository">Le repository pour les opérations liées aux artistes.</param>
+    public class DashboardService(ITitreRepository titreRepository, IStyleRepository styleRepository, IArtisteRepository artisteRepository) : IDashboardService
     {
-        ITitreRepository _titreRepository;
-        IStyleRepository _styleRepository;
-        IArtisteRepository _artisteRepository;
-        public DashboardService(ITitreRepository titreRepository, IStyleRepository styleRepository, IArtisteRepository artisteRepository)
-        {
-            _titreRepository = titreRepository;
-            _styleRepository = styleRepository;
-            _artisteRepository = artisteRepository;
-        }
+        private readonly ITitreRepository titreRepository = titreRepository;
+        private readonly IStyleRepository styleRepository = styleRepository;
+        private readonly IArtisteRepository artisteRepository = artisteRepository;
 
         /// <inheritdoc />
         public Artiste FindArtisteLePlusChronique()
         {
-            return _artisteRepository.FindAll().Where(a => a.Titres != null && a.Titres.Count != 0)
-                .OrderByDescending(a => a.Titres.Sum(t => t.Chronique != null ? 1 : 0))
+            return this.artisteRepository.FindAll()
+                .Where(a => a.Titres != null && a.Titres.Count != 0)
+                .OrderByDescending(a => a.Titres?.Sum(t => t.Chronique != null ? 1 : 0))
                 .First();
         }
 
         /// <inheritdoc />
         public Artiste FindArtisteLePlusTitresAlbumDistinct()
         {
-            return _artisteRepository.FindAll().Where(a => a.Titres != null && a.Titres.Count != 0)
-                .OrderByDescending(a => a.Titres
-                .GroupBy(t => new { t.Album, t.Artiste })
-                .Count())
+            return this.artisteRepository.FindAll()
+                .Where(a => a.Titres != null && a.Titres.Count != 0)
+                .OrderByDescending(a => a.Titres?.Count)
                 .First();
         }
 
         /// <inheritdoc />
         public Titre FindTitreLePlusLu()
         {
-            return _titreRepository.FindAll()
+            return this.titreRepository.FindAll()
                 .OrderByDescending(t => t.NbLectures)
                 .First();
         }
@@ -45,38 +49,42 @@ namespace Webzine.Business
         /// <inheritdoc />
         public int NombreArtistes()
         {
-            return _artisteRepository.FindAll().Count();
+            return this.artisteRepository.FindAll()
+                .Count();
         }
 
         /// <inheritdoc />
         public int NombreBiographiesArtistes()
         {
-            return _artisteRepository.FindAll().Count(a => !string.IsNullOrEmpty(a.Biographie));
+            return this.artisteRepository.FindAll()
+                .Count(a => !string.IsNullOrEmpty(a.Biographie));
         }
 
         /// <inheritdoc />
         public int NombreLectures()
         {
-            return _titreRepository.FindAll().Sum(t => t.NbLectures);
+            return this.titreRepository.FindAll()
+                .Sum(t => t.NbLectures);
         }
 
         /// <inheritdoc />
         public int NombreLikes()
         {
-            return _titreRepository.FindAll()
+            return this.titreRepository.FindAll()
                 .Sum(t => t.NbLikes);
         }
 
         /// <inheritdoc />
         public int NombreStyles()
         {
-            return _styleRepository.FindAll().Count();
+            return this.styleRepository.FindAll()
+                .Count();
         }
 
         /// <inheritdoc />
         public int NombreTitres()
         {
-            return _titreRepository.FindAll()
+            return this.titreRepository.FindAll()
                 .Count();
         }
     }
