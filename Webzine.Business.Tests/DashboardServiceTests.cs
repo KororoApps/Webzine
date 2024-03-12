bd27df9430126bdb7af87acc1370b57eaa75107e
KororoApps <elodie.sponton@yahoo.fr>
2024-03-12 10:43:53 +0100

    #426. DashboardServiceTests. Correction du code et ajout de commentaires.

--- Webzine.Business.Tests/DashboardServiceTests.cs
+++ Webzine.Business.Tests/DashboardServiceTests.cs
@@ -7,15 +7,20 @@ namespace Webzine.Business.Tests
     using Webzine.Entity;
     using Webzine.Entity.Fixtures;
     using Webzine.Repository;
-    using Webzine.Repository.Contracts;
 
+    /// <summary>
+    /// Tests unitaires pour la classe DashboardService.
+    /// </summary>
     [TestClass]
     public class DashboardServiceTests
     {
-        private readonly LocalTitreRepository titreRepository = new LocalTitreRepository();
-        private readonly LocalStyleRepository styleRepository = new LocalStyleRepository();
-        private readonly LocalArtisteRepository artisteRepository = new LocalArtisteRepository();
+        private readonly LocalTitreRepository titreRepository = new();
+        private readonly LocalStyleRepository styleRepository = new();
+        private readonly LocalArtisteRepository artisteRepository = new();
 
+        /// <summary>
+        /// Initialisation des données de test.
+        /// </summary>
         [TestInitialize]
         public void Initialize()
         {
@@ -25,13 +30,13 @@ namespace Webzine.Business.Tests
 
             var artistes = new List<Artiste>
             {
-                new Artiste
+                new()
                 {
                     IdArtiste = 1,
                     Nom = "Keane",
                     Biographie = "Bio 1",
-                    Titres = new List<Titre>
-                    {
+                    Titres =
+                    [
                         new Titre
                         {
                             Libelle = "Smowhere only we know",
@@ -39,8 +44,8 @@ namespace Webzine.Business.Tests
                             Album = "Keane",
                             Chronique = "Chronique 1",
                             Artiste = new Artiste { IdArtiste = 1, Nom = "Keane" },
-                            Styles = new List<Style>
-                            {
+                            Styles =
+                            [
                                 new Style{
                                     IdStyle = 1,
                                     Libelle = "Pop",
@@ -50,7 +55,7 @@ namespace Webzine.Business.Tests
                                     IdStyle = 2,
                                     Libelle = "Rock",
                                 },
-                            },
+                            ],
                             NbLectures = 5,
                             NbLikes = 2,
                         },
@@ -61,24 +66,24 @@ namespace Webzine.Business.Tests
                             Album = "Keane",
                             Chronique = "Chronique 2",
                             Artiste = new Artiste { IdArtiste = 1, Nom = "Keane" },
-                            Styles = new List<Style>
-                            {
+                            Styles =
+                            [
                                 new Style{
                                     IdStyle = 3,
                                     Libelle = "Metal",
                                 },
-                            },
+                            ],
                             NbLectures = 10,
                             NbLikes = 4,
                         },
-                    },
+                    ],
                 },
-                new Artiste
+                new()
                 {
                     IdArtiste = 3,
                     Nom = "Porter Robinson",
-                    Titres = new List<Titre>
-                    {
+                    Titres =
+                    [
                         new Titre
                         {
                             Libelle = "Everything Goes On",
@@ -86,160 +91,194 @@ namespace Webzine.Business.Tests
                             Album = "Gardiens des étoiles",
                             Chronique = "Chronique 1",
                             Artiste = new Artiste { IdArtiste = 3, Nom = "Porter Robinson" },
-                            Styles = new List<Style>
-                            {
+                            Styles =
+                            [
                                 new Style{
                                     IdStyle = 4,
                                     Libelle = "Lyric",
                                 },
-                            },
+                            ],
                             NbLectures = 30,
                             NbLikes = 15,
                         },
-                    },
+                    ],
                 },
             };
 
+            // Ajout des éléments crées dans le Repository
             foreach (var artiste in artistes)
             {
                 this.artisteRepository.Add(artiste);
 
-                foreach (var titre in artiste.Titres)
+                if (artiste.Titres != null)
                 {
-                    this.titreRepository.Add(titre);
-
-                    foreach (var style in titre.Styles)
+                    foreach (var titre in artiste.Titres)
                     {
-                        this.styleRepository.Add(style);
+                        this.titreRepository.Add(titre);
+
+                        if (titre.Styles != null)
+                        {
+                            foreach (var style in titre.Styles)
+                            {
+                                this.styleRepository.Add(style);
+                            }
+                        }
                     }
                 }
             }
         }
 
-    [TestMethod]
-    public void TestFindArtisteLePlusChronique()
-    {
-        // Arrange
-        var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
+        /// <summary>
+        /// Teste la méthode FindArtisteLePlusChronique de la classe DashboardService.
+        /// </summary>
+        [TestMethod]
+        public void TestFindArtisteLePlusChronique()
+        {
+            // Arrange
+            var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
 
-        // Act
-        var artisteLePlusChronique = dashboardService.FindArtisteLePlusChronique();
+            // Act
+            var artisteLePlusChronique = dashboardService.FindArtisteLePlusChronique();
 
-        // Assert
-        Assert.IsNotNull(artisteLePlusChronique);
-        Assert.AreEqual("Keane", artisteLePlusChronique.Nom);
+            // Assert
+            Assert.IsNotNull(artisteLePlusChronique);
+            Assert.AreEqual("Keane", artisteLePlusChronique.Nom);
+        }
+
+        /// <summary>
+        /// Teste la méthode FindArtisteLePlusTitresAlbumDistinct de la classe DashboardService.
+        /// </summary>
+        [TestMethod]
+        public void TestFindArtisteLePlusTitresAlbumDistinct()
+        {
+            // Arrange
+            var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
+
+            // Act
+            var artisteLePlusTitresAlbumDistinct = dashboardService.FindArtisteLePlusTitresAlbumDistinct();
+
+            // Assert
+            Assert.IsNotNull(artisteLePlusTitresAlbumDistinct);
+            Assert.AreEqual("Keane", artisteLePlusTitresAlbumDistinct.Nom);
+        }
+
+        /// <summary>
+        /// Teste la méthode FindTitreLePlusLu de la classe DashboardService.
+        /// </summary>
+        [TestMethod]
+        public void TestFindTitreLePlusLu()
+        {
+            // Arrange
+            var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
+
+            // Act
+            var titreLePlusLu = dashboardService.FindTitreLePlusLu();
+
+            // Assert
+            Assert.IsNotNull(titreLePlusLu);
+            Assert.AreEqual("Everything Goes On", titreLePlusLu.Libelle);
+        }
+
+        /// <summary>
+        /// Teste la méthode NombreArtistes de la classe DashboardService.
+        /// </summary>
+        [TestMethod]
+        public void TestNombreArtistes()
+        {
+            // Arrange
+            var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
+
+            // Act
+            var nombreArtistes = dashboardService.NombreArtistes();
+
+            // Assert
+            Assert.IsNotNull(nombreArtistes);
+            Assert.AreEqual(2, nombreArtistes);
+        }
+
+        /// <summary>
+        /// Teste la méthode NombreBiographiesArtistes de la classe DashboardService.
+        /// </summary>
+        [TestMethod]
+        public void TestNombreBiographiesArtistes()
+        {
+            // Arrange
+            var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
+
+            // Act
+            var nombreBiographieArtistes = dashboardService.NombreBiographiesArtistes();
+
+            // Assert
+            Assert.IsNotNull(nombreBiographieArtistes);
+            Assert.AreEqual(1, nombreBiographieArtistes);
+        }
+
+        /// <summary>
+        /// Teste la méthode NombreLectures de la classe DashboardService.
+        /// </summary>
+        [TestMethod]
+        public void TestNombreLectures()
+        {
+            // Arrange
+            var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
+
+            // Act
+            var nombreLectures = dashboardService.NombreLectures();
+
+            // Assert
+            Assert.IsNotNull(nombreLectures);
+            Assert.AreEqual(45, nombreLectures);
+        }
+
+        /// <summary>
+        /// Teste la méthode NombreLikes de la classe DashboardService.
+        /// </summary>
+        [TestMethod]
+        public void TestNombreLikes()
+        {
+            // Arrange
+            var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
+
+            // Act
+            var nombreLikes = dashboardService.NombreLikes();
+
+            // Assert
+            Assert.IsNotNull(nombreLikes);
+            Assert.AreEqual(21, nombreLikes);
+        }
+
+        /// <summary>
+        /// Teste la méthode NombreStyles de la classe DashboardService.
+        /// </summary>
+        [TestMethod]
+        public void TestNombreStyles()
+        {
+            // Arrange
+            var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
+
+            // Act
+            var nombreStyles = dashboardService.NombreStyles();
+
+            // Assert
+            Assert.IsNotNull(nombreStyles);
+            Assert.AreEqual(4, nombreStyles);
+        }
+
+        /// <summary>
+        /// Teste la méthode NombreTitres de la classe DashboardService.
+        /// </summary>
+        [TestMethod]
+        public void TestNombreTitres()
+        {
+            // Arrange
+            var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
+
+            // Act
+            var nombreTitres = dashboardService.NombreTitres();
+
+            // Assert
+            Assert.IsNotNull(nombreTitres);
+            Assert.AreEqual(3, nombreTitres);
+        }
     }
-
-    [TestMethod]
-    public void TestFindArtisteLePlusTitresAlbumDistinct()
-    {
-        // Arrange
-        var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
-
-        // Act
-        var artisteLePlusTitresAlbumDistinct = dashboardService.FindArtisteLePlusTitresAlbumDistinct();
-
-        // Assert
-        Assert.IsNotNull(artisteLePlusTitresAlbumDistinct);
-        Assert.AreEqual("Keane", artisteLePlusTitresAlbumDistinct.Nom);
-    }
-
-    [TestMethod]
-    public void TestFindTitreLePlusLu()
-    {
-        // Arrange
-        var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
-
-        // Act
-        var titreLePlusLu = dashboardService.FindTitreLePlusLu();
-
-        // Assert
-        Assert.IsNotNull(titreLePlusLu);
-        Assert.AreEqual("Everything Goes On", titreLePlusLu.Libelle);
-    }
-
-    [TestMethod]
-    public void TestNombreArtistes()
-    {
-        // Arrange
-        var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
-
-        // Act
-        var nombreArtistes = dashboardService.NombreArtistes();
-
-        // Assert
-        Assert.IsNotNull(nombreArtistes);
-        Assert.AreEqual(2, nombreArtistes);
-    }
-
-    [TestMethod]
-    public void TestNombreBiographiesArtistes()
-    {
-        // Arrange
-        var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
-
-        // Act
-        var nombreBiographieArtistes = dashboardService.NombreBiographiesArtistes();
-
-        // Assert
-        Assert.IsNotNull(nombreBiographieArtistes);
-        Assert.AreEqual(1, nombreBiographieArtistes);
-    }
-
-    [TestMethod]
-    public void TestNombreLectures()
-    {
-        // Arrange
-        var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
-
-        // Act
-        var NombreLectures = dashboardService.NombreLectures();
-
-        // Assert
-        Assert.IsNotNull(NombreLectures);
-        Assert.AreEqual(45, NombreLectures);
-    }
-
-    [TestMethod]
-    public void TestNombreLikes()
-    {
-        // Arrange
-        var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
-
-        // Act
-        var NombreLikes = dashboardService.NombreLikes();
-
-        // Assert
-        Assert.IsNotNull(NombreLikes);
-        Assert.AreEqual(21, NombreLikes);
-    }
-
-    [TestMethod]
-    public void TestNombreStyles()
-    {
-        // Arrange
-        var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
-
-        // Act
-        var NombreStyles = dashboardService.NombreStyles();
-
-        // Assert
-        Assert.IsNotNull(NombreStyles);
-        Assert.AreEqual(4, NombreStyles);
-    }
-
-    [TestMethod]
-    public void TestNombreTitres()
-    {
-        // Arrange
-        var dashboardService = new DashboardService(this.titreRepository, this.styleRepository, this.artisteRepository);
-
-        // Act
-        var NombreTitres = dashboardService.NombreTitres();
-
-        // Assert
-        Assert.IsNotNull(NombreTitres);
-        Assert.AreEqual(3, NombreTitres);
-    }
-}
 }
